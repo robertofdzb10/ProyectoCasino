d14c93ebc4fdb450ce4d9df636254acbda243b29
Anartz <Anartz@DESKTOP-381JAFS.mshome.net>
2020-12-27 14:05:10 +0100

    apuestas tabla

--- Casino/src/apuestas/Carreras.java
+++ Casino/src/apuestas/Carreras.java
@@ -12,23 +12,59 @@ import javax.swing.JPanel;
  * Carreras, un "tipo" de apuesta con posiciones en opcion de apuesta
  */
 public class Carreras extends JFrame implements Apuestas{
-	ArrayList<CaballoCarrera> participantes = new ArrayList<CaballoCarrera>();
+	ArrayList<ArrayList<CaballoCarrera> > carreras = new ArrayList<ArrayList<CaballoCarrera> >();
+	ArrayList<CaballoCarrera> participantes1 = new ArrayList<CaballoCarrera>();
+	ArrayList<CaballoCarrera> participantes2 = new ArrayList<CaballoCarrera>();
+	ArrayList<CaballoCarrera> participantes3 = new ArrayList<CaballoCarrera>();
 	
 	public void apuestasPosibles() {
-		CaballoCarrera p1 = new CaballoCarrera(4/15,"Justify",(float) 1.3);
-		CaballoCarrera p2 = new CaballoCarrera(1/6,"Secretariat",2);
-		CaballoCarrera p3 = new CaballoCarrera(1/6,"Frankel",2);
-		CaballoCarrera p4 = new CaballoCarrera(1/6,"Cigar",2);
-		CaballoCarrera p5 = new CaballoCarrera(1/10,"Seabiscuit",3);
-		CaballoCarrera p6 = new CaballoCarrera(1/30,"Citation",10);
-		CaballoCarrera p7 = new CaballoCarrera(1/10,"Tierr",3);
-		participantes.add(p1);
-		participantes.add(p2);
-		participantes.add(p3);
-		participantes.add(p4);
-		participantes.add(p5);
-		participantes.add(p6);
-		participantes.add(p7);
+		CaballoCarrera p11 = new CaballoCarrera(4/15,"Justify",(float) 1.3);
+		CaballoCarrera p12 = new CaballoCarrera(1/6,"Secretariat",2);
+		CaballoCarrera p13 = new CaballoCarrera(1/6,"Frankel",2);
+		CaballoCarrera p14 = new CaballoCarrera(1/6,"Cigar",2);
+		CaballoCarrera p15 = new CaballoCarrera(1/10,"Seabiscuit",3);
+		CaballoCarrera p16 = new CaballoCarrera(1/30,"Citation",10);
+		CaballoCarrera p17 = new CaballoCarrera(1/10,"Tierry",3);
+		participantes1.add(p11);
+		participantes1.add(p12);
+		participantes1.add(p13);
+		participantes1.add(p14);
+		participantes1.add(p15);
+		participantes1.add(p16);
+		participantes1.add(p17);
+		
+		CaballoCarrera p21 = new CaballoCarrera(1/10,"ManOfWar",3);
+		CaballoCarrera p22 = new CaballoCarrera(1/6,"Henry",2);
+		CaballoCarrera p23 = new CaballoCarrera(1/30,"Admiral",10);
+		CaballoCarrera p24 = new CaballoCarrera(1/10,"Ruffian",3);
+		CaballoCarrera p25 = new CaballoCarrera(4/15,"Brew",(float) 1.3);
+		CaballoCarrera p26 = new CaballoCarrera(1/6,"Spectacular",2);
+		CaballoCarrera p27 = new CaballoCarrera(1/6,"Jones",2);
+		participantes2.add(p21);
+		participantes2.add(p22);
+		participantes2.add(p23);
+		participantes2.add(p24);
+		participantes2.add(p25);
+		participantes2.add(p26);
+		participantes2.add(p27);
+		
+		CaballoCarrera p31 = new CaballoCarrera(1/30,"Brave",10);
+		CaballoCarrera p32 = new CaballoCarrera(1/10,"Fire",3);
+		CaballoCarrera p33 = new CaballoCarrera(1/6,"Water",2);
+		CaballoCarrera p34 = new CaballoCarrera(1/10,"Wind",3);
+		CaballoCarrera p35 = new CaballoCarrera(1/6,"Snow",2);
+		CaballoCarrera p36 = new CaballoCarrera(4/15,"Earth",(float) 1.3);
+		CaballoCarrera p37 = new CaballoCarrera(1/6,"Hurricane",2);
+		participantes3.add(p31);
+		participantes3.add(p32);
+		participantes3.add(p33);
+		participantes3.add(p34);
+		participantes3.add(p35);
+		participantes3.add(p36);
+		participantes3.add(p37);
+		carreras.add(participantes1);
+		carreras.add(participantes2);
+		carreras.add(participantes3);
 	}
 	
 	public void ventana() {
@@ -45,7 +81,12 @@ public class Carreras extends JFrame implements Apuestas{
 		etiqueta.setForeground(Color.ORANGE);
 		etiqueta.setFont(new Font("arial",Font.BOLD, 28));
 		panel.add(etiqueta);
+		
+		
+		
 		c.setVisible(true);
+		
+		
 		
 	}
 	
--- Casino/src/apuestas/Deportes.java
+++ Casino/src/apuestas/Deportes.java
@@ -1,12 +1,18 @@
 package apuestas;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import ventanas.MenuJuegos;
 
@@ -14,23 +20,12 @@ import ventanas.MenuJuegos;
  * Deportes un tipo de apuesta donde habra multiples posibilidades de apuesta
  */
 public class Deportes extends JFrame implements Apuestas {
-	ArrayList<Partido> partidos = new ArrayList<Partido>();
+	ArrayList<String []> partidos = new ArrayList<String []>();
 
 public void apuestasPosibles() {
 
-	Equipo Equipo1 = new Equipo("Leicester",0);
-	Equipo Equipo2 = new Equipo("ManUnited",0);
-	Equipo Equipo3 = new Equipo("AstonVilla",0);
-	Equipo Equipo4 = new Equipo("CrystalPalace",0);
-	Equipo Equipo5 = new Equipo("ManCity",0);
-	Equipo Equipo6 = new Equipo("Newcastle",0);
 	
-	Partido partido1 = new Partido(Equipo1,Equipo2,null,"13:30","2020-12-26",2,(float)2.2,3,String.valueOf(Equipo1.getGoles()) + "-" + String.valueOf(Equipo2.getGoles()));
-	Partido partido2 = new Partido(Equipo3,Equipo4,null,"16:00","2020-12-26",(float)1.7,(float)2.45,(float)2.3,String.valueOf(Equipo3.getGoles()) + "-" + String.valueOf(Equipo4.getGoles()));
-	Partido partido3 = new Partido(Equipo5,Equipo6,null,"21:00","2020-12-26",(float)1.3,(float)2.6,(float)2.5,String.valueOf(Equipo5.getGoles()) + "-" + String.valueOf(Equipo6.getGoles()));
-	partidos.add(partido1);
-	partidos.add(partido2);
-	partidos.add(partido3);
+
 }
 
 public void ventana() {
@@ -48,6 +43,65 @@ public void ventana() {
 	etiqueta.setForeground(Color.ORANGE);
 	etiqueta.setFont(new Font("arial",Font.BOLD, 35));
 	panel.add(etiqueta);
+	
+	String [] nombresColumnas = {"Equipo1","Equipo2","Ganador","Hora","Fecha","CuotaEquipo1","CuotaEmpate","CuotaEquipo2"};
+	Object [] [] datosFila = {
+			{"Leicester","ManUnited","null","13:30","2020-12-26","2","2.2","3"},
+			{"AstonVilla","CrystalPalace","null","16:00","2020-12-26","1.7","2.45","2.3"},
+			{"ManCity","Newcastle","null","21:00","2020-12-26","1.3","2.6","2.5"}
+	};
+	
+	JTable tabla = new JTable(datosFila,nombresColumnas);
+	add(new JScrollPane(tabla),BorderLayout.CENTER);
+	final JButton boton1 = new JButton("APUESTAS DEPORTIVAS");
+
+	ActionListener escuchador = new ActionListener() {
+		
+		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Thread Hilo = new Thread() {
+					public void run() {
+
+						Deportes d = new Deportes();
+						d.ventana();
+						
+						
+					}
+					
+				};
+				Hilo.start();
+				
+			
+				
+				
+			
+		}
+	};
+	boton1.addActionListener(escuchador);
+	panel.add(boton1);
+	final JButton boton = new JButton("Imprimir");
+	boton.setSize(175, 30);
+	boton.setLocation(135, 440);
+	boton.setBackground(Color.GRAY);
+	boton.setForeground(Color.BLUE);
+	boton.addActionListener(new ActionListener(){
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				tabla.print(getGraphics());
+			}catch(Exception e2){
+				e2.printStackTrace();
+			}
+			
+		}
+		
+	});
+	
+
+	panel.add(new JScrollPane(tabla));
+	panel.add(boton);
 	d.setVisible(true);
 }
 
