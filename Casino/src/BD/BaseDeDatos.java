f643ba753e96785be7fa96526d6afe14b3dcf4fa
robertofdzb10 <61292228+robertofdzb10@users.noreply.github.com>
2021-01-18 17:52:00 +0100

    Base de datos

---
+++ Casino/src/BD/BaseDeDatos.java
@@ -0,0 +1,55 @@
+package BD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+/**Clase responsable de la creación de la Base de datos.
+ */
+public class BaseDeDatos {
+	
+	private Connection con; // Conexión con la base de datos
+	
+	
+	/**Construye un objeto para gestionar la base de datos
+	 * En el caso de error a la hora de crear la Base de Datos, saca una ventana emergente indicando que ha habido un fallo.
+	 */
+	public BaseDeDatos() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "Error en la creación de la Base de Datos", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	
+	/**Establece conexión con la base de datos
+	 * En el caso de error a la hora de establecer conexión con la Base de Datos, saca una ventana emergente indicando que ha habido un fallo.
+	 */
+	public void establecerConexion() {
+		try {
+			con = DriverManager.getConnection("jdbc:sqlite:Casino.db");
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "No se pudo encotrar la Base de Datos Casino.db", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	
+	/**Cierra la conexión con la base de datos. La conexión debe estar abierta para poder cerrarla.
+	 * En el caso de error a la hora de cerrar conexión con la Base de Datos, saca una ventana emergente indicando que ha habido un fallo.
+	 */
+	public void cerrarConexion() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "No se pudo desconectar correctamente de la base de datos", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+}	
